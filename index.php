932d9c6416e52e0b0f6fc2814fdf702027996171
Xanthophile10 <130322036+Xanthophile10@users.noreply.github.com>
2023-11-16 14:59:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 - https://www.wysiwygwebbuilder.com">
+<link href="Final_Project.css" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+</head>
+<body>
+
+<div id="Layer1" style="position:absolute;text-align:left;left:0px;top:0px;width:1350px;height:720px;z-index:5;">
+<picture id="wb_Picture1" style="position:absolute;left:0px;top:0px;width:1350px;height:720px;z-index:0">
+<img src="images/test.png" id="Picture1" alt="" width="1350" height="759">
+</picture>
+<div id="wb_Shape1" style="position:absolute;left:851px;top:160px;width:400px;height:400px;z-index:1;">
+<img src="images/img0001.png" id="Shape1" alt="" width="400" height="400" style="width:400px;height:400px;"></div>
+<a id="registration_form_id" href="./registration_form_page.php" style="position:absolute;left:901px;top:226px;width:300px;height:75px;z-index:2;">Registration Form</a>
+<a id="staff_login_id" href="./staff_login_page.php" style="position:absolute;left:901px;top:415px;width:300px;height:75px;z-index:3;">Staff Login</a>
+</div>
+</body>
+</html>
---
+++ staff_login_page.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 - https://www.wysiwygwebbuilder.com">
+<link href="font-awesome.min.css" rel="stylesheet">
+<link href="Final_Project.css" rel="stylesheet">
+<link href="staff_login_page.css" rel="stylesheet">
+</head>
+<body>
+<div id="wb_Form1" style="position:absolute;left:0px;top:0px;width:1350px;height:720px;z-index:9;">
+<form name="Form1" method="post" action="login.php" enctype="multipart/form-data" id="Form1">
+<picture id="wb_Picture1" style="position:absolute;left:0px;top:0px;width:1350px;height:720px;z-index:0">
+<img src="images/test.png" id="Picture1" alt="" width="1350" height="759">
+</picture>
+<div id="wb_Shape1" style="position:absolute;left:475px;top:160px;width:400px;height:400px;z-index:1;">
+<img src="images/img0002.png" id="Shape1" alt="" width="400" height="400" style="width:400px;height:400px;"></div>
+<div id="wb_Text1" style="position:absolute;left:505px;top:194px;width:341px;height:30px;text-align:center;z-index:2;">
+<span style="color:#000000;font-family:Arial;font-size:27px;"><strong>BDO STAFF LOGIN</strong></span></div>
+<div id="wb_Text2" style="position:absolute;left:493px;top:234px;width:250px;height:60px;z-index:3;">
+<span style="color:#000000;font-family:Arial;font-size:27px;"><strong>USERNAME<br></strong></span></div>
+<input type="text" id="username_id" style="position:absolute;left:493px;top:268px;width:350px;height:40px;z-index:4;" name="username" value="" spellcheck="false">
+<div id="wb_Text3" style="position:absolute;left:493px;top:326px;width:250px;height:30px;z-index:5;">
+<span style="color:#000000;font-family:Arial;font-size:27px;"><strong>PASSWORD</strong></span></div>
+<input type="password" id="password_id" style="position:absolute;left:493px;top:356px;width:350px;height:40px;z-index:6;" name="password" value="" spellcheck="false">
+<input type="submit" id="login_id" name="login" value="LOGIN" style="position:absolute;left:615px;top:437px;width:120px;height:50px;z-index:7;">
+<div id="wb_IconFont1" style="position:absolute;left:829px;top:162px;width:40px;height:40px;text-align:center;z-index:8;">
+<a href="./index.php"><div id="IconFont1"><i class="fa fa-times"></i></div></a></div>
+</form>
+</div>
+</body>
+</html>
